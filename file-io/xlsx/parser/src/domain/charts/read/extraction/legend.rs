934ee583d6357900a36842418974ce151f71e149
gyyang <gyyang@users.noreply.github.com>
2026-06-18 12:29:11 -0700

    Preserve legend entry text formatting

--- file-io/xlsx/parser/src/domain/charts/read/extraction/legend.rs
+++ file-io/xlsx/parser/src/domain/charts/read/extraction/legend.rs
@@ -57,3 +57,52 @@ pub(super) fn extract_legend_from_chart_space(
 }
 
 // Extract axes from ChartSpace.
+
+#[cfg(test)]
+mod tests {
+    use super::extract_legend_from_chart_space;
+
+    #[test]
+    fn legend_entry_text_properties_extract_as_entry_format() {
+        let tx_pr = crate::domain::charts::parse_text_body(
+            br#"<c:txPr xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart"
+                       xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
+                <a:bodyPr rot="2520000"/>
+                <a:lstStyle/>
+                <a:p>
+                    <a:pPr>
+                        <a:defRPr b="1" i="1" sz="4200" u="sng" strike="sngStrike">
+                            <a:solidFill><a:srgbClr val="4472C4"/></a:solidFill>
+                            <a:latin typeface="Mog chart contract"/>
+                        </a:defRPr>
+                    </a:pPr>
+                </a:p>
+            </c:txPr>"#,
+        );
+        let chart_space = ooxml_types::charts::ChartSpace {
+            chart: ooxml_types::charts::Chart {
+                legend: Some(ooxml_types::charts::Legend {
+                    legend_entry: vec![ooxml_types::charts::LegendEntry {
+                        idx: 0,
+                        tx_pr: Some(tx_pr),
+                        ..Default::default()
+                    }],
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let legend = extract_legend_from_chart_space(&chart_space).unwrap();
+        let entries = legend.entries.as_ref().unwrap();
+        let format = entries[0].format.as_ref().unwrap();
+        let font = format.font.as_ref().unwrap();
+
+        assert_eq!(format.text_rotation, Some(42.0));
+        assert_eq!(font.bold, Some(true));
+        assert_eq!(font.italic, Some(true));
+        assert_eq!(font.size, Some(42.0));
+        assert_eq!(font.name.as_deref(), Some("Mog chart contract"));
+    }
+}
